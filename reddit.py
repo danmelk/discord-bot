b9ac174ce6200e003b5420cc34fa2e0966e55f4c
dan <danilmelkozerov0@gmail.com>
2022-05-29 11:59:12 +0500

    minor fixes

--- reddit.py
+++ reddit.py
@@ -17,14 +17,9 @@ async def data(subreddit_name):
     reddit = await auth()
     subreddit = await reddit.subreddit(subreddit_name, fetch=True)
     submission = await subreddit.random()
-    # print(submission.permalink)
     data = [subreddit, submission.permalink, submission.url, submission.title, submission.selftext, submission.score, submission.author, submission.num_comments, submission.is_self]
     titles = ['subreddit', 'url', 'media', 'title', 'body', 'score', 'author', 'num_comments', 'is_self']
     result = dict(zip(titles, data))
-    # print(result)
     return result 
 
         
-
-
-# asyncio.run(data('aww'))
