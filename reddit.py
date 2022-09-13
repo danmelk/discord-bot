3ec3f0310b324d19bd68692b72f0ff49858dc7ca
dan <danilmelkozerov0@gmail.com>
2022-09-13 05:17:54 +0500

    faster reposnse

--- reddit.py
+++ reddit.py
@@ -1,26 +1,21 @@
-import asyncio
+from contextlib import redirect_stderr
 import asyncpraw
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
-async def auth():
-    reddit = asyncpraw.Reddit(
-        client_id = os.getenv("REDDIT_ID"),
-        client_secret = os.getenv("REDDIT_SECRET"),
-        user_agent = os.getenv("REDDIT_AGENT"),
-    )
-    print(f'instance is {reddit}')
-    return reddit
+reddit = asyncpraw.Reddit(
+    client_id = os.getenv("REDDIT_ID"),
+    client_secret = os.getenv("REDDIT_SECRET"),
+    user_agent = os.getenv("REDDIT_AGENT"),)
 
 async def data(subreddit_name):
-    reddit = await auth()
+    print(f"instance is {reddit}")
     subreddit = await reddit.subreddit(subreddit_name, fetch=True)
     submission = await subreddit.random()
     data = [subreddit, submission.permalink, submission.url, submission.title, submission.selftext, submission.score, submission.author, submission.num_comments, submission.is_self]
     titles = ['subreddit', 'url', 'media', 'title', 'body', 'score', 'author', 'num_comments', 'is_self']
     result = dict(zip(titles, data))
-    await reddit.close()
     return result 
 
         
