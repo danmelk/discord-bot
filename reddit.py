684f270cd64491baa62210950479230183ca6d51
dan <danilmelkozerov0@gmail.com>
2022-09-17 19:33:47 +0500

    quickfix

--- reddit.py
+++ reddit.py
@@ -10,11 +10,11 @@ reddit = asyncpraw.Reddit(
     user_agent = os.getenv("REDDIT_AGENT"),)
 
 async def data(subreddit_name):
-    print(f"instance is {reddit}")
     subreddit = await reddit.subreddit(subreddit_name, fetch=True)
     submission = await subreddit.random()
     data = [subreddit, submission.permalink, submission.url, submission.title, submission.selftext, submission.score, submission.author, submission.num_comments, submission.is_self]
     titles = ['subreddit', 'url', 'media', 'title', 'body', 'score', 'author', 'num_comments', 'is_self']
+    print(submission.url)
     result = dict(zip(titles, data))
     return result 
 
